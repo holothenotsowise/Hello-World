e4a5e7d9f4cb135ebd4395e8f28c93206c6a6d65
Holo the not so wise wolf <holothenotsowise@gmail.com>
2017-02-14 20:44:40 -0800

    Create helloyori.cpp

---
+++ helloyori.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+std::cout << "Hello Yori!!!" << std::endl;
+int love = 1000;
+if(love == 1000)
+{
+std::cout << "All teh luvs for le yori" << std::endl;
+}
+return 0;
+}
